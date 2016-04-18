10d24c767bb570a72bb2eaf5e536d2babeaad345
Anjack <gary.reif@gmail.com>
2016-04-18 11:07:13 -0400

    Added findEmptySpacenearPlayer function so we can place the chest in an empty spot.
    New function will scan an area ~20x20 units centered on the player
    position before death. The first unoccupied spot will be returned as a
    position which can be passed to the create_entity method.

--- control.lua
+++ control.lua
@@ -11,7 +11,7 @@ function movePlayerItems (src, dst)
 	for n, c in pairs(src.get_contents()) do
 		item = { name = n, count = c }
 		if dst.can_insert(item) == true then
-			printf(item.name .. " : " .. item.count)
+			-- printf(item.name .. " : " .. item.count)
 			dst.insert(item)
 		else
 			printf("Error moving items - dst is full or cannot accept")
@@ -19,11 +19,34 @@ function movePlayerItems (src, dst)
 	end
 end
 
+-- Will scan a grid around player position looking for an open spot.
+function findEmptySpaceNearPlayer(player)
+	-- printf("Debug entity name: " .. player.name .. " at " .. player.position["x"] .. ", " .. player.position["y"])
+	-- Why does this not work? Assigning these values to these variables fails silently.
+	-- local px = player.position["x'"]
+	-- local py = player.position["y"]
+	for offsetX = -10, 10 do
+		for offsetY = -10, 10 do
+			-- Cycle through each space within ~10 units of player. Check for 0 sized array return from find_entities
+			if #player.surface.find_entities({{player.position["x"] + offsetX, player.position["y"] + offsetY}, {player.position["x"] + offsetX, player.position["y"] + offsetY}}) == 0 then
+				return {player.position["x"] + offsetX, player.position["y"] + offsetY}
+			end
+		end
+	end
+	return nil
+end
+
 script.on_event(defines.events.on_entity_died, function(event)
 	if event.entity.name == "player" then
 		local player = event.entity
+		local targetPos = findEmptySpaceNearPlayer(player)
+		
+		if targetPos == nil then
+			printf("Nowhere to spawn chest.")
+			return
+		end
 		-- Create the corpse-chest
-		local cChest = player.surface.create_entity({name = "corpse-chest", position = event.entity.position, force = game.forces.neutral})
+		local cChest = player.surface.create_entity({name = "corpse-chest", position = targetPos, force = game.forces.neutral})
 		
 		if cChest == nil then
 			printf("Corpse spawn - Failed.")
