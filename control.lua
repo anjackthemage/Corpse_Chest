4369794906e486fe7c46cb9eae01333ff2388f77
Anjack <gary.reif@gmail.com>
2016-04-17 22:29:20 -0400

    Fixed crash when failed to place corpse chest due to entity already at position
    Bad error handling on the part of factorio's engine. Need to force/kill
    what's in the spot before place. this at least prevents the crash for
    now.

--- control.lua
+++ control.lua
@@ -1,13 +1,15 @@
 require "defines"
 
 script.on_event(defines.events.on_entity_died, function(event)
-	if event.entity.name == "player" then
+	if event.entity.name == "player" and event.entity.surface.can_place_entity{name = "corpse-chest", position = event.entity.position} then
 		event.entity.surface.create_entity{name = "corpse-chest", position = event.entity.position, force = game.forces.neutral}
 		local theCorpse = event.entity.surface.find_entity('corpse-chest', event.entity.position)
-		for index = 1, 2 do
+		for index = 1, 6 do
 			for itemName, itemCount in pairs(event.entity.get_inventory(index).get_contents()) do
 				if theCorpse.can_insert{name = itemName, count = itemCount} then
 					theCorpse.insert{name = itemName, count = itemCount}
+				else
+					game.show_message_dialog{text={"Failed to insert " .. itemName .. ": " .. itemCount}}
 				end
 			end
 		end
